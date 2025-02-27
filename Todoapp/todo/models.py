8c93439bc132459456e8d78fc9be836d3f31bc4b
Misiriya-Yousuf <misiriyayousuf369@gmail.com>
2025-02-27 17:22:02 +0530

    models defined

--- Todoapp/todo/models.py
+++ Todoapp/todo/models.py
@@ -5,6 +5,7 @@ class Todo(models.Model):
     description = models.TextField(blank=True, null=True)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return self.title
